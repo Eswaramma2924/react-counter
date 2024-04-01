e732a7b6f094afe14da60c75a6ca463ba50ae92c
Your Name <eswarigutti04@gmail.com>
2024-04-01 17:29:50 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Message from "./components/Message"
 
 function App() {
+  
+  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    console.log("I am inside useEffect")
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center bg-yellow-500 justify-center h-screen gap-10">
+      <h1 className="text-6xl">{count}</h1>
+      <div className="flex items-center justify-normal gap-4">
+      <button type="button" className="border border-blue-500 bg-blue-200 px-4 py-2 rounded-lg"
+      onClick={() => {
+        setCount(count+1)
+      }}> Increment </button>
+      <button type="button" className="border border-blue-500 bg-blue-200 px-4 py-2 rounded-lg"
+      onClick={() => {
+        setCount(count-1)
+      }}> decrement </button>
+      
+      </div>
+      {count === 10 ? <Message msg1="I have reached 10 now. Stop increments." /> : <Message msg1="I am not yet 10. Keep going."/>}
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = ({msg1}) => {
+  return (
+    <div>
+      {msg1}
+    </div>
+  );
+}
+
+export default Message;
